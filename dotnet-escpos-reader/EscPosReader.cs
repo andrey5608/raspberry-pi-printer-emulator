61ef80a8f39c388de9dda5cc7dfa24e7d027000d
Andrey Petrov <andrey5608@gmail.com>
2022-08-21 15:51:53 +0200

    Save binary data to a file

--- dotnet-escpos-reader/EscPosReader.cs
+++ dotnet-escpos-reader/EscPosReader.cs
@@ -27,10 +27,13 @@ public class EcsPosReader
             {
                 var existingData = sp.ReadByte();
                 receivedSymbolsAsInt.Add(existingData);
-                Console.WriteLine(String.Join(", ", receivedSymbolsAsInt.ToArray()));
                 if (Enumerable.SequenceEqual(GetThreeLast(receivedSymbolsAsInt), cutPaperCommand))
                 {
+                    Console.WriteLine(String.Join(", ", receivedSymbolsAsInt.ToArray()));
                     Console.WriteLine("Paper cut");
+                    // send to decode
+                    SaveBinaryFile(receivedSymbolsAsInt);
+                    receivedSymbolsAsInt = new List<int>();
                     break;
                 }
             }
@@ -39,5 +42,14 @@ public class EcsPosReader
     private static List<int> GetThreeLast(List<int> list)
     {
         return Enumerable.Reverse(list).Take(3).Reverse().ToList();
+    }
+
+    private static void SaveBinaryFile(List<int> inputList){
+        var filePath = @"./out.bin";
+        var binaryOutput = inputList.SelectMany(BitConverter.GetBytes).ToArray();
+        using (var writer = new BinaryWriter(File.OpenWrite(filePath))){
+            writer.Write(binaryOutput);
+        }
+        Console.WriteLine("Successfully created bin file");
     }
 }
