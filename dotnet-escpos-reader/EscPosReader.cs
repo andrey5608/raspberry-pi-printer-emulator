6f1afb93efbf9a0427e57c177a7a03b5966230df
Andrey Petrov <andrey5608@gmail.com>
2022-08-21 14:19:05 +0200

    Paper cut and clear formatting

--- dotnet-escpos-reader/EscPosReader.cs
+++ dotnet-escpos-reader/EscPosReader.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.IO.Ports;
 using System.Text;
+using System.Linq;
 
 public class EcsPosReader
 {
     static void Main(string[] args)
     {
-        
-         // Attach a method to be called when there
-         // is data waiting in the port's buffer 
-        // Begin communications 
         Console.WriteLine("Incoming Data:");
         using (SerialPort sp = new SerialPort("/dev/serial0",
       9600))
         {
-            //sp.Encoding = Encoding.UTF8;
             sp.Encoding = Encoding.GetEncoding("ibm850");
             sp.BaudRate = 9600;
             //sp.ReadTimeout = 1000;
@@ -23,44 +19,24 @@ public class EcsPosReader
 
             Console.WriteLine("Type Ctrl-C to exit...");
             int i = 0;
+            var receivedSymbolsAsInt = new List<int>();
+            var cutPaperCommand = new List<int> { 27, 61, 0 };
 
-            while (i < 100)
+            while (true)
             {
-                // if RATE is set to really high Arduino may fail to respond in time
-                // then on the next command you might get an old message
-                // ReadExisting will read everything from the internal buffer
                 var existingData = sp.ReadByte();
-                Console.WriteLine(existingData);
-                /*
-                if (!existingData.Contains('\n') && !existingData.Contains('\r'))
+                receivedSymbolsAsInt.Add(existingData);
+                Console.WriteLine(String.Join(", ", receivedSymbolsAsInt.ToArray()));
+                if (Enumerable.SequenceEqual(GetThreeLast(receivedSymbolsAsInt), cutPaperCommand))
                 {
-                    // we didn't get the response yet, let's wait for it then
-                    try
-                    {
-                        Console.WriteLine(sp.ReadLine());
-                    }
-                    catch (TimeoutException)
-                    {
-                        Console.WriteLine($"ERROR: No response in {sp.ReadTimeout}ms.");
-                    }
-                }*/
+                    Console.WriteLine("Paper cut");
+                    break;
+                }
             }
         };
-        // Enter an application loop to keep this thread alive 
-        //Console.ReadLine();
-        /*
-        if (args.Length == 0)
-        {
-            Console.WriteLine("arduino-demo <portName> [<baudRate>=9600]");
-            return;
-        }
-
-        // to get port name you can use SerialPort.GetPortNames()
-        string portName = args[0];
-        int baudRate = args.Length >= 2 ? int.Parse(args[1]) : 9600;
-
-        
-
-        */
-    } 
+    }
+    private static List<int> GetThreeLast(List<int> list)
+    {
+        return Enumerable.Reverse(list).Take(3).Reverse().ToList();
+    }
 }
